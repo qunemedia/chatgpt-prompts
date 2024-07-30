2196d5a5dc578947536e25e52d4a55aedc05eec4
Daniel Kussin <daniel.kussin@kussin.de>
2024-07-30 19:32:00 +0200

    This commit refs #64863: Added empty language templates

---
+++ src/translations/zh_TW.php
@@ -0,0 +1,41 @@
+<?php
+
+$aTranslations = array(
+    "LABEL___en_US" => "",
+    "LABEL___de_DE" => "",
+    "LABEL___fr_FR" => "",
+    "LABEL___es_ES" => "",
+    "LABEL___pt_PT" => "",
+    "LABEL___it_IT" => "",
+    "LABEL___nl_NL" => "",
+    "LABEL___pl_PL" => "",
+    "LABEL___tr_TR" => "",
+    "LABEL___ru_RU" => "",
+    "LABEL___uk_UA" => "",
+    "LABEL___zh_CN" => "",
+    "LABEL___zh_TW" => "Chinese ",
+
+    'CONTEXT' => ',
+
+    'LONG_DESCRIPTION' => '',
+    'SHORT_DESCRIPTION' => '',
+    'PRODUCT_SEARCHKEYS' => '',
+    'PRODUCT_ATTRIBUTES' => '',
+    'CATGEORY_LONG_DESCRIPTION' => '',
+    'CATGEORY_SHORT_DESCRIPTION' => '',
+    'MANUFACTURER_LONG_DESCRIPTION' => '',
+    'MANUFACTURER_SHORT_DESCRIPTION' => '',
+    'VENDOR_SHORT_DESCRIPTION' => '',
+
+    'TRANSLATION_TITLE' => '',
+    'TRANSLATION_LONG_TRANSLATION' => '',
+    'TRANSLATION_SHORT_DESCRIPTION' => '',
+
+    'OPTIMIZE_CONTENT' => '',
+
+    'ENHANCED_ARTICLE_DATA' => '',
+
+    'LONG_DESCRIPTION_INSTRUCTION' => implode(PHP_EOL, array(
+        
+    )),
+);
